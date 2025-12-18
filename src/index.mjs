92f324a7f4a5a350aecb95107055e5360058d89f
Mohamed Benzineb <mhmdhayza@gmail.com>
2025-12-18 22:16:09 +0100

    Route params

--- src/index.mjs
+++ src/index.mjs
@@ -4,15 +4,30 @@ const app = express();
 
 const Port = process.env.Port || 3000 ;
 
+const users =   [ { id: 1 , name : "Benzineb Mohamed" , age : 23 , insta : "mhs_dz" }
+                , { id: 2 , name : "laila" , age : 22 , insta : "Unknown" }
+                , { id: 3 , name : "meriem" , age : 22 , insta : "Unknown" }
+                , { id: 4 , name : "futureMe" , age : 25 , insta : "Startup" }
+            ];
+
 app.get( "/" , ( req , res ) => {
     res.status(201).send( { msg : "Hello, World!"} );
 })
 
 app.get( "/api/users" , ( req , res ) => {
-    res.status(201).send( { users : [ { id: 1 , name : "Benzineb Mohamed" , age : 23 , insta : "mhs_dz" }
-                                    , { id: 2 , name : "laila" , age : 22 , insta : "Unknown" }
-    ] } );
+    res.status(201).send( { users } );
 })
+
+app.get( "/api/users/:id" , ( req , res ) => {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) return res.status(400).send( { msg : "Invalid Request"} );
+    const user = users.find( user => user.id === id );
+    if( !user ) return res.status(404).send( { msg : "User not found"} );
+    res.status(201).send( `The user id is : ${user.name}
+                            /// his name is : ${user.name}
+                            /// his age is : ${user.age} 
+                            /// his  insta is : ${user.insta}`);
+});
 
 app.get( "/api/product" , ( req , res ) => {
     res.status(201).send( { product : [ { id: 1 , name : "Laptop" , price : 80000 , brand : "Dell" }
