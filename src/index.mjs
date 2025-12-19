3f721c36f3d9baa7fdfef9ce3308047fc027cc4f
Mohamed Benzineb <mhmdhayza@gmail.com>
2025-12-19 18:33:04 +0100

    query params

--- src/index.mjs
+++ src/index.mjs
@@ -4,10 +4,13 @@ const app = express();
 
 const Port = process.env.Port || 3000 ;
 
-const users =   [ { id: 1 , name : "Benzineb Mohamed" , age : 23 , insta : "mhs_dz" }
-                , { id: 2 , name : "laila" , age : 22 , insta : "Unknown" }
-                , { id: 3 , name : "meriem" , age : 22 , insta : "Unknown" }
-                , { id: 4 , name : "futureMe" , age : 25 , insta : "Startup" }
+const users =   [ { id: 1 , username : "Benzineb Mohamed" , age : 23 , insta : "mhs_dz" }
+                , { id: 2 , username : "laila" , age : 22 , insta : "Unknown" }
+                , { id: 3 , username : "meriem" , age : 22 , insta : "Unknown" }
+                , { id: 4 , username : "futureMe" , age : 25 , insta : "Startup" }
+                , { id : 5 , username : "ahmed" , age : 24 , insta : "Unknown" }
+                , { id : 6 , username : "alaa" , age : 20 , insta : "Unknown" }
+                , { id : 7 , username : "mohamed" , age : 23 , insta : "Unknown" }
             ];
 
 app.get( "/" , ( req , res ) => {
@@ -18,13 +21,24 @@ app.get( "/api/users" , ( req , res ) => {
     res.status(201).send( { users } );
 })
 
+app.get( "/api/usersm" , ( req , res ) => {
+    console.log(req.query);
+    const { query : { filter , value } } = req;
+    
+    if( filter && value ) {
+        return res.status(201).send(users.filter( (user) => user[filter].includes(value )) );
+    }
+    return res.status(201).send( { users } );
+})
+
+
 app.get( "/api/users/:id" , ( req , res ) => {
     const id = parseInt(req.params.id);
     if (isNaN(id)) return res.status(400).send( { msg : "Invalid Request"} );
     const user = users.find( user => user.id === id );
     if( !user ) return res.status(404).send( { msg : "User not found"} );
-    res.status(201).send( `The user id is : ${user.name}
-                            /// his name is : ${user.name}
+    res.status(201).send( `The user id is : ${user.id}
+                            /// his name is : ${user.username}
                             /// his age is : ${user.age} 
                             /// his  insta is : ${user.insta}`);
 });
